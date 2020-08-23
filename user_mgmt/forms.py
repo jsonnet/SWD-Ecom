3668af327ed90767c36fcf55c0e074afe1441f30
Joshua Sonnet <sonnet.joshua@gmail.com>
2020-08-23 16:16:50 +0200

    Auto stash before merge of "master" and "origin/master"

--- user_mgmt/forms.py
+++ user_mgmt/forms.py
@@ -1,4 +1,3 @@
-from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 from user_mgmt.models import UserProfile
@@ -6,17 +5,12 @@ from django.contrib.auth.models import User
 
 
 class UserRegisterForm(UserCreationForm):
-
-    def __init__(self, *args, **kwargs):
-        super(UserRegisterForm, self).__init__(*args, **kwargs)
-
-        for fieldname in ['password1', 'password2']:
-            self.fields[fieldname].help_text = None
-        
-
-
+    # def __init__(self, *args, **kwargs):
+    #     super(UserRegisterForm, self).__init__(*args, **kwargs)
+    #
+    #     for fieldname in ['password1', 'password2']:
+    #         self.fields[fieldname].help_text = None
 
     class Meta:
         model = UserProfile
         fields = ['username', 'first_name', 'last_name', 'password1', 'password2']
-
