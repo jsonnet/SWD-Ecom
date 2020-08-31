5429ea77a535b2f8a8aa071bc9a751cefe0489ae
Guido <s8gubatt@stud.uni-saarland.de>
2020-08-31 16:17:29 +0200

    separate app

---
+++ shop/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+#P4
+#TODO discuss if Cascade is best option for deletion
+
+class Payment(models.Model):
+    pk_x = models.AutoField(primary_key=True, db_column='pk')  # pk is a reserved word in python!
+    amount = models.IntegerField()
+    method = models.CharField(max_length=32)
+
+class Address(models.Model):
+    pk_x = models.AutoField(primary_key=True, db_column='pk')  # pk is a reserved word in python!
+    user = models.CharField(max_length=30)
+    street = models.CharField(max_length=64)
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=32)
+    country = models.CharField(max_length=64)
+    additional_info = models.CharField(max_length=64)
+
+class Order(models.Model):
+    pk_x = models.AutoField(primary_key=True, db_column='pk')  # pk is a reserved word in python!
+    customer_id = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    placed = models.BooleanField(default=False)
+    date_placed = models.DateTimeField()
+    shipping_address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
+
+class CartItem(models.Model):
+    pk_x = models.AutoField(primary_key=True, db_column='pk')  # pk is a reserved word in python!
+    product_id = models.CharField(max_length=64)
+    quantity = models.IntegerField()
+    order_id = models.ForeignKey(Order,on_delete=models.CASCADE)
+
+
---
+++ shop/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from shop_mgmt import views
+
+urlpatterns = [
+    path('products/list', views.product_list, name='product_list'), 
+    path('basket/{str:order_id}', views.basket),  
+    path('checkout/{str:order_id}', views.product_details)
+]
