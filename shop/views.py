e33a0fa6ce0ab140b7c4c5d08b5f0fb1402014ef
Guido <s8gubatt@stud.uni-saarland.de>
2020-09-01 00:48:28 +0200

    product list and popup

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from models import *
+from shop.models import *
+from shop_mgmt.models import *
 
 def product_list(request):
 
@@ -8,5 +9,18 @@ def product_list(request):
 
     return render(request, 'product_list.html', {'products':products})
 
+def basket(request, order_id):
+
+    # get all products from shop and requesting partner
+    products = Product.objects.all()
+
+    return render(request, 'product_list.html', {'products':products})
+
+def product_details(request, order_id):
+
+    # get all products from shop and requesting partner
+    products = Product.objects.all()
+
+    return render(request, 'product_list.html', {'products':products})
 
 
