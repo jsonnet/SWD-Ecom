79f14dd0f539ce4069c2f5a4eb51c4f5e636f81b
Joshua Sonnet <sonnet.joshua@gmail.com>
2020-09-01 18:04:48 +0200

    Let's begin with some functionality
    - [x] formatting
    - [x] nicer product_list (shop front)
    - [ ] TODO Details view
    - [ ] TODO add to basket function
    - [ ] TODO basket and checkout view

--- shop/views.py
+++ shop/views.py
@@ -2,25 +2,23 @@ from django.shortcuts import render
 from shop.models import *
 from shop_mgmt.models import *
 
-def product_list(request):
 
+def product_list(request):
     # get all products from shop and requesting partner
     products = Product.objects.all()
 
-    return render(request, 'product_list.html', {'products':products})
+    return render(request, 'product_list.html', {'products': products})
+
 
 def basket(request, order_id):
-
     # get all products from shop and requesting partner
     products = Product.objects.all()
 
-    return render(request, 'product_list.html', {'products':products})
+    return render(request, 'product_list.html', {'products': products})
+
 
 def product_details(request, order_id):
-
     # get all products from shop and requesting partner
     products = Product.objects.all()
 
-    return render(request, 'product_list.html', {'products':products})
-
-
+    return render(request, 'product_list.html', {'products': products})
