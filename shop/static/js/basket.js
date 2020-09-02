91ff9c7ea3ef328a333b381dea337382bdf4d8e1
Joshua Sonnet <sonnet.joshua@gmail.com>
2020-09-02 19:31:44 +0200

    More work has been done
    - [x] added removeBasket function TODO implement
    - [x] template cleanup and comments
    - [x] image and button got some logic
    - [x] added seller to details
    - [x] added product image to basket
    - [x] fixed sso login redirect for next parameter
    
    TODO check code for work
    TODO implement checkout
    TODO implement total in basket

--- shop/static/js/basket.js
+++ shop/static/js/basket.js
@@ -1,25 +1,31 @@
 function addbasket(product_id, product_price) {
 
-	var xhr = new XMLHttpRequest()
+    var xhr = new XMLHttpRequest()
 
-	xhr.onreadystatechange = function() {
-	if (this.readyState === this.DONE) {
-	           if (this.responseText == "login") {
-		   	window.location.href = '/accounts/login-redirect?next=/shop/add-basket/'+product_id+'?redirect';
-		   } else {
-		   	console.log(this.responseText)
-		   }
-	}
-	}
+    xhr.onreadystatechange = function () {
+        if (this.readyState === this.DONE) {
+            if (this.responseText === "login") {
+                window.location.href = '/accounts/login/?next=/shop/add-basket/' + product_id;
+            } else {
+                console.log(this.responseText)
+            }
+        }
+    }
 
-	xhr.open("GET", "/shop/add-basket/"+product_id) 
-	xhr.send()
+    xhr.open("GET", "/shop/add-basket/" + product_id)
+    xhr.send()
 
-	//update total amount
-	amount = document.getElementById('baskettotal').textContent
-	amount = parseInt(amount)
-	amount = amount || 0
+    //FIXME
+    //update total amount
+    amount = document.getElementById('baskettotal').textContent
+    amount = parseInt(amount)
+    amount = amount || 0
 
-	amount += product_price 
-	document.getElementById('baskettotal').textContent = amount
+    amount += product_price
+    document.getElementById('baskettotal').textContent = amount
+}
+
+function removeBasket(product_id){
+    //TODO implement
+    console.log("This should remove " + product_id)
 }
--- shop/views.py
+++ shop/views.py
@@ -12,64 +12,63 @@ def product_list(request):
 
 
 def basket(request, order_id):
-
     items = []
     if request.user.is_authenticated:
         try:
-            order = Order.objects.get(customer_id=request.user, placed=False) 
+            order = Order.objects.get(customer_id=request.user, placed=False)
             try:
-                #iterate all items from that order
+                # iterate all items from that order
                 cartitems = CartItem.objects.filter(order_id=order)
 
                 for cartitem in cartitems:
-                    basketitem = dict() 
-                    product = Product.objects.get(slug=cartitem.product_id) 
+                    basketitem = dict()
+                    product = Product.objects.get(slug=cartitem.product_id)
 
-                    #add to separate structure that combines everything
+                    # add to separate structure that combines everything
                     basketitem['price'] = product.price
                     basketitem['name'] = product.name
+                    basketitem['image'] = product.image
                     basketitem['quantity'] = cartitem.quantity
+                    basketitem['slug'] = product.slug
+
                     items.append(basketitem)
 
             except CartItem.DoesNotExist:
                 pass
         except Order.DoesNotExist:
             pass
-    return render(request, 'basket.html', {'items':items})
+    return render(request, 'basket.html', {'items': items})
 
 
 def checkout(request, order_id):
-
     return render(request, 'checkout.html', None)
 
+
 def add_basket(request, product_id):
-    
     if request.user.is_authenticated:
         order = Order()
 
-       #create new order if not exists
+        # create new order if not exists
         try:
-            order = Order.objects.get(customer_id=request.user, placed=False) 
+            order = Order.objects.get(customer_id=request.user, placed=False)
         except Order.DoesNotExist:
             order.customer_id = request.user
             order.save()
-        
-        
-        #check if item exists already
+
+        # check if item exists already
         item = CartItem()
-        try: 
-            item = CartItem.objects.get(product_id=product_id, order_id=order) 
+        try:
+            item = CartItem.objects.get(product_id=product_id, order_id=order)
         except CartItem.DoesNotExist:
-            item.quantity = 0 
+            item.quantity = 0
 
         item.product_id = product_id
-        item.quantity = item.quantity+1
+        item.quantity = item.quantity + 1
         item.order_id = order
 
         item.save()
-        return HttpResponse('success', content_type="text/plain") 
+        return HttpResponse('success', content_type="text/plain")
     else:
-        return HttpResponse('login', content_type="text/plain") 
+        return HttpResponse('login', content_type="text/plain")
 
-    return HttpResponse('loginn', content_type="text/plain") 
-     
+    return HttpResponse('loginn', content_type="text/plain")
