191caa5b9d36b7b0baf54f7ef2bcddeb23debe31
Guido <s8gubatt@stud.uni-saarland.de>
2020-09-02 13:34:23 +0200

    adding to total amount of basket

--- shop/static/js/basket.js
+++ shop/static/js/basket.js
@@ -1,13 +1,23 @@
-function addbasket(product_id) {
+function addbasket(product_id, product_price) {
 
 	var xhr = new XMLHttpRequest()
 
 	xhr.onreadystatechange = function() {
 	if (this.readyState === this.DONE) {
-	            console.log(this.responseText)
+	           if (this.responseText == "login") {
+		   	window.location.href = '/accounts/login/?next=/shop/add-basket/'+product_id;
+		   }
 	}
 	}
 
 	xhr.open("GET", "/shop/add-basket/"+product_id) 
 	xhr.send()
+
+	//update total amount
+	amount = document.getElementById('baskettotal').textContent
+	amount = parseInt(amount)
+	amount = amount || 0
+
+	amount += product_price 
+	document.getElementById('baskettotal').textContent = amount
 }
