7b010ac7cf18492329d8fa4bebed50faf40177b9
Joshua Sonnet <sonnet.joshua@gmail.com>
2020-09-03 17:48:55 +0200

    Checkout should work now
    - [x] Added check to fill out address
    - [x] added redirect after checkout
    - [x] Objects now get created

--- shop/views.py
+++ shop/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from django.utils import timezone
 
 from shop.models import *
 from shop_mgmt.models import *
@@ -24,40 +25,59 @@ def basket(request, order_id):
                   {'items': items, 'item_total': total_count, 'order_id': order_id, 'placed': already_placed})
 
 
-# FIXME need default function same as basket!!
 @login_required
 def checkout(request, order_id):
     # If we actually checkout
     if request.method == "POST":
-        # TODO create a new address object and save
-        #  create a new payment object
-        #  add both to the order object
-        #  change placed and time in order object
-        #  send success message
-        #  also check again if every product was still available
         params = json.loads(request.body.decode('utf-8'))
         print(json.loads(request.body.decode('utf-8')))
         print(request.user)
 
         street = params.get('street')
-        add_info = params.get('additional_info')
+        add_info = params.get('additional_info').strip()
         zip_city = params.get('city')  # needs to be split
         country = params.get('country')
-        payment = params.get('payment')
+        payment = params.get('payment')  # had to be set
         amount = calc_total(order_id, request.user)
+        items = params.get('items')  # shouldn't be empty
 
-        # TODO create objects
+        # Creating objects and set in relation to
+        pay = Payment(amount=float(amount), method=payment)
+        pay.save()
 
-        messages.error(request,
-                       'One or more items are currently not available anymore')  # this will show on the next request
+        address = Address(user=request.user, street=street, city=zip_city.split(' ', 1)[1],
+                          zip_code=zip_city.split(' ', 1)[0], country=country, additional_info=add_info)
+        address.save()
 
+        # deduct products
+        for i in items:
+            p = Product.objects.get(slug=i)
+            if p.count - items[i] < 0:
+                messages.error(request,
+                               'One or more items are currently not available anymore')  # this will show on the next request
+                return HttpResponse("error", status=410)
+            p.count -= items[i]
+            p.save()
+
+        # finish order
+        order = Order.objects.get(customer_id=request.user, pk_x=order_id)
+        order.shipping_address = address
+        order.payment = pay
+        order.placed = True
+        order.date_placed = timezone.now()
+        order.save()
+
+        messages.success(request, 'Thanks for your purchase')
         return HttpResponse("success")
 
     items, total_count, placed = get_basket_items(order_id, request)
     address = None
     try:
-        address = Address.objects.get(user=request.user)
+        address = Address.objects.filter(user=request.user)
+        address = address.reverse()[0]  # once we have multiple addresses use the last one
     except Address.DoesNotExist:
+        pass
+    except IndexError:
         pass
 
     return render(request, 'checkout.html',
