048b2d7d578a818f83de40d5e2784f7670d0f0b2
Joshua Sonnet <sonnet.joshua@gmail.com>
2020-09-03 23:18:16 +0200

    Templates for public and private site filled
    - [x] added confidential data to private site
    - [x] public shop will now show u-<id> products
    TODO next up google analytics

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,8 +1,31 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-def private_profile(request,email):
-    return render(request, 'private_profile.html', {})
+from shop.models import Address, Order
+from shop_mgmt.models import Product
+from user_mgmt.models import UserProfile
 
+
+@login_required
+def private_profile(request, email):
+    if str(request.user) == email:
+        # FIXME check empty for order and address
+        orders = Order.objects.filter(customer_id=request.user)
+
+        address = Address.objects.filter(user=request.user)
+        address = address.reverse()[0]  # once we have multiple addresses use the last one
+
+        profile = UserProfile.objects.get(username=email)
+
+        return render(request, 'private_profile.html', {'orders': orders, 'address': address, 'profile': profile})
+
+    return HttpResponse(status=401)
+
+
+@login_required
 def public_store(request, email):
-    return render(request, 'public_shop.html', {})
+
+    products = Product.objects.filter(seller=f'u-{email}')
+
+    return render(request, 'public_shop.html', {'products': products})
