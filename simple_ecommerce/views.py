46f163a3d98a8c5ab684a67d1ea05d82d9cb65d1
Guido <s8gubatt@stud.uni-saarland.de>
2020-09-04 12:25:39 +0200

    removed xframe options and fixed unicode bug

--- simple_ecommerce/views.py
+++ simple_ecommerce/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 
 def index(request):
     return render(request, 'base.html')  
 
+@xframe_options_exempt
 def high_air(request):
     return render(request, 'high_air.html')
